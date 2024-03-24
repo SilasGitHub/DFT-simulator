da9e8aee9b192b62880222b9adc74dc0be010c09
Silas <s.degraaf@student.utwente.nl>
2024-03-24 16:13:59 +0100

    Select basic events and highlight them
    Reorderable list in the toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
+import { useState } from "react";
 import FlowDisplay from "./components/FlowDisplay";
 import Toolbar from "./components/Toolbar";
 import './css/main.css'
 
 export default function App() {
+	const [selected, setSelected] = useState(["hi", "bye", "try", "sky"]); 
+
 	return (
 	<div style={{ width: '100vw', height: '100vh', display: "flex"}}>
 		<div>
-			<Toolbar/>	
+			<Toolbar selected={selected} setSelected={setSelected}/>	
 		</div>
 		{/* <div> */}
-			<FlowDisplay />
+			<FlowDisplay selected={selected} setSelected={setSelected}/>
 		{/* </div> */}
-	</div>
+    </div>
 	);
 }
