216beed170c5c0991f3642d7c4c0096c4e82e098
Migush <Migushthe2nd@users.noreply.github.com>
2024-03-25 17:21:17 +0100

    Allow connecting only once to target
    Fixed #29

--- src/components/CustomHandle.tsx
+++ src/components/CustomHandle.tsx
@@ -1,38 +1,82 @@
-import {HTMLAttributes, useMemo} from "react"
-import {getConnectedEdges, Handle, HandleProps, ReactFlowState, useNodeId, useStore} from "reactflow"
-import {NodeUnion} from "./nodes/Nodes.ts"
+import React, {HTMLAttributes, useMemo} from "react"
+import {
+    Connection,
+    getConnectedEdges,
+    Handle,
+    HandleProps,
+    ReactFlowState,
+    useNodeId,
+    useNodes,
+    useStore,
+} from "reactflow"
+import {useNodeUtils} from "../utils/useNodeUtils.tsx"
 
 // Taken from: https://reactflow.dev/examples/nodes/connection-limit
 
-const selector = (state: ReactFlowState) => ({
-    nodeInternals: state.nodeInternals,
-    edges: state.edges,
-})
+const selector = (state: ReactFlowState) => (state)
 
 type CustomHandleProps = {
-    isConnectable: boolean | number
-} & Omit<HandleProps & Omit<HTMLAttributes<HTMLDivElement>, 'id'>, "isConnectable">
+    isConnectable: boolean | number,
+    /**
+     * Which types of nodes should be able to connect to this handle
+     */
+    // allowedNodeConnectionTypes?: NodeType[],
+} & Omit<HandleProps & Omit<HTMLAttributes<HTMLDivElement>, "id">, "isConnectable">
 
-export default function CustomHandle(props: CustomHandleProps) {
-    const {nodeInternals, edges} = useStore(selector)
+export default function CustomHandle({isConnectable, ...rest}: CustomHandleProps) {
+    const {nodeInternals, edges, ...state} = useStore(selector)
+    const {getNodeById, getOutgoingNodesAndEdges} = useNodeUtils(useNodes(), edges)
     const nodeId = useNodeId()
+
 
     const isHandleConnectable = useMemo(() => {
         if (!nodeId) return false
 
-        if (typeof props.isConnectable === "number") {
-            const node = nodeInternals.get(nodeId) as NodeUnion
+        if (typeof isConnectable === "number") {
+            const node = getNodeById(nodeId)
+            if (!node) return false
             const connectedEdges = getConnectedEdges([node], edges).filter((value) => {
-                return value.targetHandle === props.id && value.target === nodeId
+                return value.targetHandle === rest.id && value.target === nodeId
             })
-            return connectedEdges.length < props.isConnectable
+            return connectedEdges.length < isConnectable
         }
 
-        return props.isConnectable
-    }, [props.isConnectable, props.id, nodeInternals, nodeId, edges])
+        return isConnectable
+    }, [isConnectable, rest.id, nodeInternals, nodeId, edges])
+
+    /**
+     * This valid connection runs on the source handle
+     */
+    const isValidConnection = React.useCallback((connection: Connection) => {
+        if (rest.id === 'dependent-1') {
+            console.log(rest)
+        }
+        // allow connecting self only once
+        const sourceNode = getNodeById(connection.source)!
+        const targetNode = getNodeById(connection.target)!
+        // const targetHandle = connection.targetHandle
+
+        const {outgoingNodes} = getOutgoingNodesAndEdges(sourceNode)
+        if (outgoingNodes.includes(targetNode)) {
+            // this node is already connected to the target node, so we don't allow the connection
+            return false
+        }
+
+        // // allow connecting only to compatible types
+        // if (!allowedNodeConnectionTypes) {
+        //     return true
+        // }
+        // return allowedNodeConnectionTypes.includes(targetNode.type as NodeType)
+        return true
+    }, [getNodeById, getOutgoingNodesAndEdges, state])
 
     return (
-        <Handle {...props as HandleProps} isConnectable={isHandleConnectable} style={{...props.style, minWidth: '10px', minHeight: '10px'}} ></Handle>
+        <Handle
+            {...rest as HandleProps}
+            isConnectable={isHandleConnectable}
+            isValidConnection={isValidConnection}
+            style={{...rest.style, minWidth: "10px", minHeight: "10px"}}
+        />
     )
 }
 
