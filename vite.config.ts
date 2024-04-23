a7aef1811615f7b016df7dc317aaec4fd5d96f49
Migush <Migushthe2nd@users.noreply.github.com>
2024-04-23 14:45:31 +0200

    Add base-url env setting

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import UnocssVitePlugin from "unocss/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: process.env.BASE_URL || "/",
     plugins: [
         UnocssVitePlugin(),
         react(),
